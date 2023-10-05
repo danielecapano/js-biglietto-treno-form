a3919bce55af8c44ddd8f0d669446165d0e98d56
danielecapano <daniele.capano@gmail.com>
2023-10-05 19:16:42 +0200

    ADD - style  and content and js bonus

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
 const PREZZO_KM = 0.21;
 const SCONTO_UNDER_18 = 20;
 const SCONTO_OVER_65 = 40;
+const elDOMName = document.getElementById("nome");
 const elDOMInput = document.getElementById("distanza");
 const elDOMSelect = document.getElementById("discount");
-const elDOMBtn = document.querySelector(".btn-calc");
-const elDOMPrice = document.querySelector(".price");
+const elDOMBtnCalc = document.querySelector(".btn-calc");
+const elDOMBtnReset = document.querySelector(".btn-reset");
 
-elDOMBtn.addEventListener("click", function () {
+const elDOMPrice = document.getElementById("price");
+const elDOMTicketName = document.getElementById("ticket-name");
+const elDOMTicketType = document.getElementById("ticket-type");
+
+const elDOMwagon = document.getElementById("wagon");
+const elDOMCpCode = document.getElementById("cp-code");
+
+elDOMBtnCalc.addEventListener("click", function () {
   const distanza = elDOMInput.value; // 100
   const age = elDOMSelect.value; // 1
   const prezzoBase = distanza * PREZZO_KM; // 21
   let sconto = 0;
+  elDOMTicketType.innerHTML = "Biglietto Standard";
+
   if (age === "0") {
     sconto = (prezzoBase * SCONTO_UNDER_18) / 100;
+    elDOMTicketType.innerHTML = "Biglietto Ridotto 20%";
   } else if (age === "2") {
     sconto = (prezzoBase * SCONTO_OVER_65) / 100;
+    elDOMTicketType.innerHTML = "Biglietto Ridotto 40%";
   }
 
   const prezzoFinale = prezzoBase - sconto;
-  console.log(prezzoBase, sconto);
-  elDOMPrice.innerHTML = prezzoFinale.toFixed(2);
+  elDOMTicketName.innerHTML = elDOMName.value;
+
+  elDOMPrice.innerHTML = prezzoFinale.toFixed(2) + " &euro;";
+
+  elDOMwagon.innerHTML = Math.floor(Math.random() * 10 + 1);
+  elDOMCpCode.innerHTML = Math.floor(Math.random() * 100000 + 1);
+});
+
+elDOMBtnReset.addEventListener("click", function () {
+  elDOMName.value = "";
+  elDOMInput.value = "";
 });
